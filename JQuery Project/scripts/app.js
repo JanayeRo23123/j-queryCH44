0c7876a4d4e41b54afd8193fd28971d845ff3d50
JanayeRo23123 <151698192+JanayeRo23123@users.noreply.github.com>
2024-01-27 13:06:12 -0600

    Add files via upload

--- JQuery Project/scripts/app.js
+++ JQuery Project/scripts/app.js
@@ -1,63 +1,33 @@
-function sayHello(name, lastName){
-    console.log("Hello "+name+" "+lastName);
+//global variables
+let iconImportant = false;
+function saveTask(){
+    console.log("Saving task");
 
 }
-
-
-function sum(num1, num2){
-    const res=num1+num2;
-    return res;
-
-}
-
-function printNumbers(){
-    //print numbers 1 to 20 
-    //except 7 and 13
-    let sum = 0;
-    let count = 0;
-
-    let numbers = [12,4,123,4567,234,56,12,87,124,865,233,788,43,91,544,782,653,845];
-    for(let i=1; i<21; i++){
-        if (i != 7 && i !=13){
-            console.log(i);
-        }
+function changeIcon(){
+    console.log("Changing icon");
+    const nonImportant = "fa-solid fa-crown";
+    const isImportant = "fa-solid fa-splotch";
+    if (iconImportant) {
+        $("#iImportant").removeClass(isImportant).addClass(nonImportant);
+        iconImportant = false;
     }
-    for (let i = 0; i < numbers.length; i++){
-        let num = numbers[i];
-        console.log(num);
-        //sum numbers
-        sum = num;
-        //only greater numbers
-        if (num > 500)
-        count += 1;
+    else{
+        $("#iImportant").removeClass(isImportant).addClass(nonImportant);
+        iconImportant = true;
+        //please try to restore the icon
     }
-    console.log("the sum of the numbers is:" + sum);
-    console.log("there are "+count+ " number bigger than 500")
-    //print every number in the list 
-    //print the sum of all numbers
-    //print how many numbers greater than 500
 }
-
 
 function init(){
+    console.log("This is the parent of everything");
+    //load data
 
-    console.log("Hello world!");
-    const x = "Janaye";
-    sayHello("Janaye, Rouser");
-    sayHello("Jane, Doe");
-    const result = sum(21+21);
-    console.log(result);
-
+    //hook events
+    $("#btnSave").click(saveTask);
+    $("#iImportant").click(changeIcon);
+    //document.getElementById("btnSave").click(saveTask)
 
 }
-//create a function that the sums of two numbers and execute it in the init
 
 window.onload = init;
-//window.onload = init(); this is a not
-
-//PRINCIPLE
-//DRY -- DO NOT REPEAT YOURSELF
-//KISS - KEEP IT SIMPLE STUPID 
-
-
-let numbers = [12,4,123,4567,234,56,12,87,124,865,233,788,43,91,544,782,653,845];
